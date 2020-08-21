13dc401aa8705b3e6dddd25f42433d7cf5d91a44
Lukas Endigo 91 <58499825+le91@users.noreply.github.com>
2020-08-21 14:47:31 +0600

    Update and rename tmux_build_from_source_Ubuntu.sh to Install_tmux_2.8_on_Ubuntu_16.04.sh

--- Install_tmux_2.8_on_Ubuntu_16.04.sh
+++ Install_tmux_2.8_on_Ubuntu_16.04.sh
@@ -1,8 +1,14 @@
 # Steps to build and install tmux from source on Ubuntu.
 # Takes < 25 seconds on EC2 env [even on a low-end config instance].
 VERSION=2.8
-sudo apt-get -y remove tmux
-sudo apt-get -y install wget tar libevent-dev libncurses-dev
+sudo apt remove -y tmux
+sudo apt install -y wget tar libevent-dev libncurses-dev
+sudo apt install -y git
+sudo apt install -y automake
+sudo apt install -y bison
+sudo apt install -y build-essential
+sudo apt install -y pkg-config
+sudo apt install -y libncurses5-dev
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
 tar xf tmux-${VERSION}.tar.gz
 rm -f tmux-${VERSION}.tar.gz
